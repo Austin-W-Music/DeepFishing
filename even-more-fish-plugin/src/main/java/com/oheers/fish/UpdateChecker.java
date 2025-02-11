220fcad87f0a6f60e4665e87e5b7924255594141
Austin-W-Music <info@austinwestphalmusic.com>
2025-02-11 15:42:45 -0500

    Update UpdateChecker.java

--- even-more-fish-plugin/src/main/java/com/oheers/fish/UpdateChecker.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/UpdateChecker.java
@@ -1,7 +1,7 @@
-package com.oheers.fish;
+package com.Austin-W-Music.fish;
 
-import com.oheers.fish.config.messages.ConfigMessage;
-import com.oheers.fish.permissions.AdminPerms;
+import com.Austin-W-Music.fish.config.messages.ConfigMessage;
+import com.Austin-W-Music.fish.permissions.AdminPerms;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -13,11 +13,11 @@ import java.util.Scanner;
 
 public class UpdateChecker {
 
-    private final EvenMoreFish plugin;
+    private final DeepFishing plugin;
     private final int resourceID;
 
 
-    public UpdateChecker(final EvenMoreFish plugin, final int resourceID) {
+    public UpdateChecker(final DeepFishing plugin, final int resourceID) {
         this.plugin = plugin;
         this.resourceID = resourceID;
     }
@@ -26,7 +26,7 @@ public class UpdateChecker {
         try (final Scanner scanner = new Scanner(new URL("https://api.spigotmc.org/simple/0.1/index.php?action=getResource&id=" + resourceID).openStream())) {
             return ((JSONObject) new JSONParser().parse(scanner.nextLine())).get("current_version").toString();
         } catch (Exception ignored) {
-            EvenMoreFish.getInstance().getLogger().warning("EvenMoreFish failed to check for updates against the spigot website, to check manually go to https://www.spigotmc.org/resources/evenmorefish.91310/updates");
+            DeepFishing.getInstance().getLogger().warning("DeepFishing failed to check for updates against the spigot website, to check manually go to https://www.spigotmc.org/resources/evenmorefish.91310/updates");
             return plugin.getDescription().getVersion();
         }
 
@@ -36,9 +36,9 @@ public class UpdateChecker {
 class UpdateNotify implements Listener {
 
     @EventHandler
-    // informs admins with emf.admin permission that the plugin needs updating
+    // informs admins with df.admin permission that the plugin needs updating
     public void playerJoin(PlayerJoinEvent event) {
-        if (!EvenMoreFish.getInstance().isUpdateAvailable()) {
+        if (!DeepFishing.getInstance().isUpdateAvailable()) {
             return;
         }
 
