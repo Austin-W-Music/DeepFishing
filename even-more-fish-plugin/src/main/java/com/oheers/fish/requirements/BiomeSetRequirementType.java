fc769e12e5e44c9b9596782d4f83f47affa1ab8a
Austin-W-Music <info@austinwestphalmusic.com>
2025-02-14 14:11:01 -0500

    Update BiomeSetRequirementType.java

--- even-more-fish-plugin/src/main/java/com/oheers/fish/requirements/BiomeSetRequirementType.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/requirements/BiomeSetRequirementType.java
@@ -1,9 +1,9 @@
-package com.oheers.fish.requirements;
+package com.Austin-W-Music.fish.requirements;
 
-import com.oheers.fish.EvenMoreFish;
-import com.oheers.fish.api.requirement.RequirementContext;
-import com.oheers.fish.api.requirement.RequirementType;
-import com.oheers.fish.config.MainConfig;
+import com.Austin-W-Music.fish.DeepFishing;
+import com.Austin-W-Music.fish.api.requirement.RequirementContext;
+import com.Austin-W-Music.fish.api.requirement.RequirementType;
+import com.Austin-W-Music.fish.config.MainConfig;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
@@ -23,12 +23,12 @@ public class BiomeSetRequirementType implements RequirementType {
             configLocation = "N/A";
         }
         if (world == null) {
-            EvenMoreFish.getInstance().getLogger().severe("Could not get world for " + configLocation + ", returning false by " +
+            DeepFishing.getInstance().getLogger().severe("Could not get world for " + configLocation + ", returning false by " +
                     "default. The player may not have been given a fish if you see this message multiple times.");
             return false;
         }
         if (location == null) {
-            EvenMoreFish.getInstance().getLogger().severe("Could not get location for " + configLocation + ", returning false by " +
+            DeepFishing.getInstance().getLogger().severe("Could not get location for " + configLocation + ", returning false by " +
                     "default. The player may not have been given a fish if you see this message multiple times.");
             return false;
         }
@@ -36,7 +36,7 @@ public class BiomeSetRequirementType implements RequirementType {
         for (String value : values) {
             @NotNull List<Biome> checkBiomes = MainConfig.getInstance().getBiomeSets().get(value);
             if (checkBiomes == null) {
-                EvenMoreFish.getInstance().getLogger().severe(value + " is not a valid biome set.");
+                DeepFishing.getInstance().getLogger().severe(value + " is not a valid biome set.");
                 continue;
             }
             if (checkBiomes.contains(hookBiome)) {
@@ -58,7 +58,7 @@ public class BiomeSetRequirementType implements RequirementType {
 
     @Override
     public @NotNull Plugin getPlugin() {
-        return EvenMoreFish.getInstance();
+        return DeepFishing.getInstance();
     }
 
 }
