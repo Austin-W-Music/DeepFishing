4d4b999725057d0562250a7d14484e286e2b430d
Austin-W-Music <info@austinwestphalmusic.com>
2025-02-14 15:03:21 -0500

    Update H2Strategy.java

--- even-more-fish-plugin/src/main/java/com/oheers/fish/database/strategies/impl/H2Strategy.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/database/strategies/impl/H2Strategy.java
@@ -1,7 +1,7 @@
-package com.oheers.fish.database.strategies.impl;
+package com.Austin-W-Music.fish.database.strategies.impl;
 
-import com.oheers.fish.config.MainConfig;
-import com.oheers.fish.database.strategies.DatabaseTypeStrategy;
+import com.Austin-W-Music.fish.config.MainConfig;
+import com.Austin-W-Music.fish.database.strategies.DatabaseTypeStrategy;
 import org.flywaydb.core.api.configuration.FluentConfiguration;
 import org.jetbrains.annotations.NotNull;
 
