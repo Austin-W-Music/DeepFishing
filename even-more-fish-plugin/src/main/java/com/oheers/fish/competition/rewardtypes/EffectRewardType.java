40983f9df75891b0bb56fd3f98f90679400063ed
Austin-W-Music <info@austinwestphalmusic.com>
2025-02-18 12:21:55 -0500

    Update EffectRewardType.java

--- even-more-fish-plugin/src/main/java/com/oheers/fish/competition/rewardtypes/EffectRewardType.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/competition/rewardtypes/EffectRewardType.java
@@ -1,7 +1,7 @@
-package com.oheers.fish.competition.rewardtypes;
+package com.Austin-W-Music.fish.competition.rewardtypes;
 
-import com.oheers.fish.EvenMoreFish;
-import com.oheers.fish.api.reward.RewardType;
+import com.Austin-W-Music.fish.DeepFishing;
+import com.Austin-W-Music.fish.api.reward.RewardType;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -17,11 +17,11 @@ public class EffectRewardType implements RewardType {
     public void doReward(@NotNull Player player, @NotNull String key, @NotNull String value, Location hookLocation) {
         String[] parsedEffect = value.split(",");
         if (parsedEffect.length < 3) {
-            EvenMoreFish.getInstance().getLogger().warning("Invalid effect specified for RewardType " + getIdentifier() + ": " + value);
+            DeepFishing.getInstance().getLogger().warning("Invalid effect specified for RewardType " + getIdentifier() + ": " + value);
             return;
         }
         // Adds a potion effect in accordance to the config.yml "EFFECT:" value
-        EvenMoreFish.getScheduler().runTask(player, () -> player.addPotionEffect(new PotionEffect(Objects.requireNonNull(PotionEffectType.getByName(parsedEffect[0])), Integer.parseInt(parsedEffect[2]) * 20, Integer.parseInt(parsedEffect[1]))));
+        DeepFishing.getScheduler().runTask(player, () -> player.addPotionEffect(new PotionEffect(Objects.requireNonNull(PotionEffectType.getByName(parsedEffect[0])), Integer.parseInt(parsedEffect[2]) * 20, Integer.parseInt(parsedEffect[1]))));
     }
 
     @Override
@@ -31,12 +31,12 @@ public class EffectRewardType implements RewardType {
 
     @Override
     public @NotNull String getAuthor() {
-        return "Oheers";
+        return "DevAustin";
     }
 
     @Override
     public @NotNull JavaPlugin getPlugin() {
-        return EvenMoreFish.getInstance();
+        return DeepFishing.getInstance();
     }
 
 }
